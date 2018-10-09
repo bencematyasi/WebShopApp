ae934f9ad91bde70de7b85811954813abee02583
bencematyasi <31878635+bencematyasi@users.noreply.github.com>
2018-10-09 10:04:40 +0200

    startUp started to implement

---
+++ WebShopApp.Infrastructure.Data/SQLRepositories/OrderRepository.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using WebShopApp.Core.Domain_Service;
+using WebShopApp.Core.Entity;
+
+namespace WebShopApp.Infrastructure.Data.Repositories
+{
+    public class OrderRepository : IOrderRepository
+    {
+        public Order CreatProduct(Order order)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Order DeleteProduct(Order deleteOrder)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Order GetOrderById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Order> ReadAllProduct()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Order UpdateProduct(Order updateOrder)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
