03fcaa9e6e39a34e55fb71884310cffc1659b4dd
bencematyasi <31878635+bencematyasi@users.noreply.github.com>
2018-10-09 10:31:03 +0200

    finished the startup and added DBInitializer

---
+++ WebShopApp.Infrastructure.Data/DBInitializer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebShopApp.Infrastructure.Data
+{
+    public class DBInitializer
+    {
+        public static void SeedDB(WebShopAppContext ctx)
+        {
+            ctx.Database.EnsureCreated();
+            ctx.Database.EnsureCreated();
+            throw new NotImplementedException();
+
+
+        }
+    }
+}
--- WepShopApp/Startup.cs
+++ WepShopApp/Startup.cs
@@ -1,30 +1,68 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using WebShopApp.Core.Application_Service.Impl;
+using WebShopApp.Core.Application_Service.Service;
+using WebShopApp.Core.Domain_Service;
+using WebShopApp.Infrastructure.Data;
+using WebShopApp.Infrastructure.Data.Repositories;
 
 namespace WepShopApp
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        //public Startup(IConfiguration configuration)
+        //{
+        //    Configuration = configuration;
+        //}
+
+        public IConfiguration _conf { get; }
+
+        private IHostingEnvironment _env { get; set; }
+
+        public Startup(IHostingEnvironment env)
         {
-            Configuration = configuration;
+            _env = env;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            _conf = builder.Build();
         }
-
-        public IConfiguration Configuration { get; }
-
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            if (_env.IsDevelopment())
+            {
+                services.AddDbContext<WebShopAppContext>(
+                    opt => opt.UseSqlite("Data Source=petShopApp.db"));
+            }
+            else if (_env.IsProduction())
+            {
+                services.AddDbContext<WebShopAppContext>(
+                    opt => opt
+                        .UseSqlServer(_conf.GetConnectionString("DefaultConnection")));
+            }
+
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<IOrderRepository, OrderRepository>();
+            services.AddScoped<IOrderService, OrderService>();
+
+
+
+            services.AddMvc().AddJsonOptions(options => {
+                options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+            });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -34,13 +72,23 @@ namespace WepShopApp
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                using (IServiceScope scope = app.ApplicationServices.CreateScope())
+                {
+                    WebShopAppContext ctx = scope.ServiceProvider.GetService<WebShopAppContext>();
+                    DBInitializer.SeedDB(ctx);
+                }
             }
             else
             {
+                using (var scope = app.ApplicationServices.CreateScope())
+                {
+                    var ctx = scope.ServiceProvider.GetService<WebShopAppContext>();
+                    ctx.Database.EnsureCreated();
+                }
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+           // app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
